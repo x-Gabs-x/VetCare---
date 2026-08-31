4dd2c73ed8f2a6ba00ed9dc0f4b2203fc02aca5d
Lucas <lucascardosocst@gmail.com>
2026-08-30 23:52:01 -0300

    feat: modulo de vacinas com registro, historico e lembretes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const petRoutes = require('./routes/petRoutes');
+const vacinaRoutes = require('./routes/vacinaRoutes');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
@@ -19,6 +20,7 @@ app.get('/', (req, res) => {
 app.use('/auth', authRoutes);
 app.use('/usuarios', usuarioRoutes);
 app.use('/pets', petRoutes);
+app.use('/vacinas', vacinaRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ erro: 'Rota nao encontrada.' });
